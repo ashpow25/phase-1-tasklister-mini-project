2781a9667dd8bf9f2cf59ff4128e636f5a89cf81
Ashley <ashpow25@gmail.com>
2021-07-31 22:54:51 -0700

    created events to add and delete tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const form = document.getElementById('create-task-form');
+const taskList = document.getElementById('tasks');
+
+//submit event form
+form.addEventListener('submit', addTask);
+
+//delete event
+taskList.addEventListener('click', removeTask);
+
+//add task
+function addTask(e){
+  e.preventDefault();
+}
 });
